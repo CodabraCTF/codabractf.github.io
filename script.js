3fa2d4c9814bb0aca68ec1bcca2ceeef9d58f993
notrurs <fursik111@mail.ru>
2019-04-20 00:12:33 +0300

    microCTF v1.0

---
+++ script.js
@@ -0,0 +1,38 @@
+var key = getRandomInt(1000,100000);
+var rand_question = getRandomInt(0,4);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function signin(){
+    var login = document.getElementById("login").value;
+    var password = document.getElementById("password").value;
+    
+	if (login == 'admin' && password == key){
+		window.open('offer.html')
+	} 
+	else if (login == '' || password == '') {
+		alert( 'Вы что-то забыли ввести' );
+	} 
+	else {
+		alert( 'Неверный логин или пароль' );
+	}
+}
+
+function forgot(){
+    question = ['Любимый цвет?','Любимая машина?','Любимое место отдыха?','На какой улице живете?','В каком городе была ваша первая работа?'];
+    answer = ['голубой','лимузин','бали','аптекарский переулок','москва'];
+    var rand = rand_question;
+    var secret = prompt(question[rand], '');
+    secret = secret.toLowerCase();
+	if (secret == answer[rand]){
+		alert('Пароль от администратора: ' + key);
+	} 
+	else if (secret == null) {
+		alert( 'Вы не ввели ответ на вопрос' );
+	} 
+	else {
+		alert( 'Неправильный ответ' );
+	}
+}
